479cdabe65d4cebd6482b512a24bbbcaaeaef935
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2021-12-27 06:37:29 -0300

    feat: remix blog with markdown files

---
+++ react-remix/my-remix-app/app/routes/posts/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "remix"
+import { useLoaderData } from "remix"
+import { Post, getPosts } from "~/post"
+
+export const loader = () => {
+  return getPosts()
+}
+
+
+export default function Posts() {
+  const posts = useLoaderData<Post[]>()
+  console.log(posts)
+  return (
+    <div>
+      <h1>Posts</h1>
+      {Array.isArray(posts) && <ul>{posts.map(post => <li key={post.slug}><Link to={post.slug}>{post.title}</Link></li>)}</ul>}
+    </div>
+  )
+}
