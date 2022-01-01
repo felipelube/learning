fdaf94c2225073c654d6a0ea214897fd9f35cc07
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-01-01 12:04:24 -0300

    chore: add eslint and make it work with prettier

---
+++ react-remix/remix-jokes/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "standard",
+    "plugin:react/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+  },
+};
