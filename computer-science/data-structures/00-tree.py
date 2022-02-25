207a2fd39f1d28a83f4201c04fb324944868347a
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-02-24 22:48:32 -0300

    refactor: helper function to simplify interface of traversal functions

--- computer-science/data-structures/00-tree.py
+++ computer-science/data-structures/00-tree.py
@@ -15,13 +15,18 @@ root.right.left = Node(40)
 root.right.right = Node(50)
 
 
-def inorder(node: Node, lyst: List[int] = []) -> List[int]:
-    if node is None:
-        return
+def inorder(root: Node) -> List[int]:
+    lyst = []
 
-    inorder(node.left)
-    lyst.append(node.k)
-    inorder(node.right)
+    def recursive(node: Node):
+        if node is None:
+            return
+
+        recursive(node.left)
+        lyst.append(node.k)
+        recursive(node.right)
+
+    recursive(root)
 
     return lyst
 
@@ -29,13 +34,18 @@ def inorder(node: Node, lyst: List[int] = []) -> List[int]:
 assert(inorder(root) == [20, 10, 40, 30, 50])
 
 
-def preorder(node: Node, lyst: List[int] = []) -> List[int]:
-    if node is None:
-        return
+def preorder(root: Node) -> List[int]:
+    lyst = []
 
-    lyst.append(node.k)
-    preorder(node.left)
-    preorder(node.right)
+    def recursive(node: Node):
+        if node is None:
+            return
+
+        lyst.append(node.k)
+        recursive(node.left)
+        recursive(node.right)
+
+    recursive(root)
 
     return lyst
 
@@ -44,13 +54,18 @@ assert(preorder(root) == [10, 20, 30, 40, 50])
 
 
 # note: post order is NOT tail recursive, so it is less optimized for python
-def postorder(node: Node, lyst=[]) -> List[int]:
-    if node is None:
-        return
+def postorder(root: Node) -> List[int]:
+    lyst = []
 
-    postorder(node.left)
-    postorder(node.right)
-    lyst.append(node.k)
+    def recursive(node: Node):
+        if node is None:
+            return
+
+        recursive(node.left)
+        recursive(node.right)
+        lyst.append(node.k)
+
+    recursive(root)
 
     return lyst
 
