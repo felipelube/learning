e064c3cd3a74b79ece01ef2a0341ce5585a37f8b
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2022-02-24 22:20:16 -0300

    feat: inorder traversal

--- computer-science/data-structures/00-tree.py
+++ computer-science/data-structures/00-tree.py
@@ -4,3 +4,23 @@ class Node:
         self.left: Node = None
         self.right: Node = None
 
+
+root = Node(10)
+root.left = Node(20)
+root.right = Node(30)
+root.right.left = Node(40)
+root.right.right = Node(50)
+
+
+def inorder(node: Node, lyst=[]):
+    if node is None:
+        return
+
+    inorder(node.left)
+    lyst.append(node.k)
+    inorder(node.right)
+
+    return lyst
+
+
+assert(inorder(root) == [20, 10, 40, 30, 50])
